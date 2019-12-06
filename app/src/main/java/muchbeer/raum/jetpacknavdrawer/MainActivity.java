9ad8c5949864a3fadfd4d8dde5e35a705d95aa09
muchbeer <gman2them@gmail.com>
2019-12-06 17:51:45 +0300

    Initial commit

---
+++ app/src/main/java/muchbeer/raum/jetpacknavdrawer/MainActivity.java
@@ -0,0 +1,101 @@
+package muchbeer.raum.jetpacknavdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public Toolbar toolbar;
+
+    public DrawerLayout drawerLayout;
+
+    public NavController navController;
+
+    public NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.navigationView);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+    //    NavigationUI.setupWithNavController(toolbar, navController);
+        //    NavigationUI.setupWithNavController(layout, toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawerLayout.openDrawer(GravityCompat.START);
+
+    /*    if(drawerLayout.isDrawerOpen(GravityCompat.START)); {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }*/
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        menuItem.setChecked(true);
+
+        drawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+
+        switch (id) {
+
+            case R.id.first:
+                navController.navigate(R.id.firstFragment);
+                break;
+
+            case R.id.second:
+                navController.navigate(R.id.secondFragment);
+                break;
+
+            case R.id.third:
+                navController.navigate(R.id.thirdFragment);
+                break;
+
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/muchbeer/raum/jetpacknavdrawer/fragment/SecondFragment.java
@@ -0,0 +1,27 @@
+package muchbeer.raum.jetpacknavdrawer.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import muchbeer.raum.jetpacknavdrawer.R;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.second_fragment, container, false);
+    }
+}
